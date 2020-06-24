6c429fc9f43b2777f63a9ded746c4c8473db5e91
Tony Suslovich <Tony.Suslovich@arup.com>
2020-06-24 14:57:23 -0400

    #78133

--- CrmPackage/WebResources/Script (JScript)/ccrm_ClientGrouping_main_library.js
+++ CrmPackage/WebResources/Script (JScript)/ccrm_ClientGrouping_main_library.js
@@ -20,55 +20,55 @@ function gridOnLoad() {
     }
 }
 
-function relationshipTeam_onChange() {
+//function relationshipTeam_onChange() {
 
-    var teamId = Xrm.Page.getAttribute('ccrm_managingteam').getValue();
-    if (teamId == null) {
+//    var teamId = Xrm.Page.getAttribute('ccrm_managingteam').getValue();
+//    if (teamId == null) {
 
-        Xrm.Page.getAttribute('ccrm_accounttype').setValue(null);
-        Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(null);
-        return;
-    }
+//        Xrm.Page.getAttribute('ccrm_accounttype').setValue(null);
+//        Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(null);
+//        return;
+//    }
 
-    teamId = teamId[0].id.replace('{', '').replace('}', '');
+//    teamId = teamId[0].id.replace('{', '').replace('}', '');
 
-    var req = new XMLHttpRequest();
-    req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.2/teams(" + teamId + ")?$select=_ccrm_relationshipmanager_value,ccrm_relationshiptype", true);
-    req.setRequestHeader("OData-MaxVersion", "4.0");
-    req.setRequestHeader("OData-Version", "4.0");
-    req.setRequestHeader("Accept", "application/json");
-    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-    req.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            req.onreadystatechange = null;
-            if (this.status === 200) {
-                var result = JSON.parse(this.response);
-                var _ccrm_relationshipmanager_value = result["_ccrm_relationshipmanager_value"];
-                var _ccrm_relationshipmanager_value_formatted = result["_ccrm_relationshipmanager_value@OData.Community.Display.V1.FormattedValue"];
-                var _ccrm_relationshipmanager_value_lookuplogicalname = result["_ccrm_relationshipmanager_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
-                var ccrm_relationshiptype = result["ccrm_relationshiptype"];
+//    var req = new XMLHttpRequest();
+//    req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.2/teams(" + teamId + ")?$select=_ccrm_relationshipmanager_value,ccrm_relationshiptype", true);
+//    req.setRequestHeader("OData-MaxVersion", "4.0");
+//    req.setRequestHeader("OData-Version", "4.0");
+//    req.setRequestHeader("Accept", "application/json");
+//    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+//    req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+//    req.onreadystatechange = function () {
+//        if (this.readyState === 4) {
+//            req.onreadystatechange = null;
+//            if (this.status === 200) {
+//                var result = JSON.parse(this.response);
+//                var _ccrm_relationshipmanager_value = result["_ccrm_relationshipmanager_value"];
+//                var _ccrm_relationshipmanager_value_formatted = result["_ccrm_relationshipmanager_value@OData.Community.Display.V1.FormattedValue"];
+//                var _ccrm_relationshipmanager_value_lookuplogicalname = result["_ccrm_relationshipmanager_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
+//                var ccrm_relationshiptype = result["ccrm_relationshiptype"];
 
-                Xrm.Page.getAttribute('ccrm_accounttype').setValue(ccrm_relationshiptype);
-                if (_ccrm_relationshipmanager_value == null) {
-                    Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(null);
-                } else {
-                    var lookup = new Array();
-                    lookup[0] = new Object();
-                    lookup[0].id = _ccrm_relationshipmanager_value;
-                    lookup[0].name = _ccrm_relationshipmanager_value_formatted;
-                    lookup[0].entityType = 'systemuser';
-                    Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(lookup);
-                }
+//                Xrm.Page.getAttribute('ccrm_accounttype').setValue(ccrm_relationshiptype);
+//                if (_ccrm_relationshipmanager_value == null) {
+//                    Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(null);
+//                } else {
+//                    var lookup = new Array();
+//                    lookup[0] = new Object();
+//                    lookup[0].id = _ccrm_relationshipmanager_value;
+//                    lookup[0].name = _ccrm_relationshipmanager_value_formatted;
+//                    lookup[0].entityType = 'systemuser';
+//                    Xrm.Page.getAttribute('ccrm_relationshipmanager').setValue(lookup);
+//                }
 
-            } else {
-                Xrm.Utility.alertDialog(this.statusText);
-            }
-        }
-    };
-    req.send();
+//            } else {
+//                Xrm.Utility.alertDialog(this.statusText);
+//            }
+//        }
+//    };
+//    req.send();
 
-}
+//}
 
 function OpenClientGroupingMatrixReport(primaryControl) {
     var formContext = primaryControl;
