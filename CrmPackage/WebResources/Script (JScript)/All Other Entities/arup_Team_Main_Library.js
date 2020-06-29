0de4624f82490aaa59404cbe42e83956cdb80f94
Sandeep Bhavanasi <Sandeep.Bhavanasi@arup.com>
2020-06-29 16:52:45 +1000

    added exit function to team main lib js file

--- CrmPackage/WebResources/Script (JScript)/All Other Entities/arup_Team_Main_Library.js
+++ CrmPackage/WebResources/Script (JScript)/All Other Entities/arup_Team_Main_Library.js
@@ -246,3 +246,56 @@ function SetClientValues(formContext) {
     if (formContext.data.getIsDirty())
         formContext.data.save();
 }
+
+// runs on Exit button
+function exitForm(primaryControl) {
+    var formContext = primaryControl;
+    //see if the form is dirty
+    var ismodified = formContext.data.entity.getIsDirty();
+    if (ismodified == false) {
+        formContext.ui.close();
+        return;
+    }
+
+    Alert.show('<font size="6" color="#FF9B1E"><b>Warning</b></font>',
+        '<font size="3" color="#000000"></br>Some fields on the form have been changed.</br>Click "Save and Exit" button to save your changes and exit.</br>Click "Exit Only" button to exit without saving.</font>',
+        [
+            {
+                label: "<b>Save and Exit</b>",
+                callback: function () {
+                    var acctAttributes = formContext.data.entity.attributes.get();
+                    var highlight = true;
+                    var cansave = true;
+                    if (acctAttributes != null) {
+                        for (var i in acctAttributes) {
+                            if (acctAttributes[i].getRequiredLevel() == 'required') {
+                                highlight = formContext.getAttribute(acctAttributes[i].getName()).getValue() != null;
+                                if (highlight == false && cansave == true) { cansave = false; }
+                            }
+                        }
+                    }
+                    if (cansave) { formContext.data.entity.save("saveandclose"); }
+                },
+                setFocus: true,
+                preventClose: false
+            },
+            {
+                label: "<b>Exit Only</b>",
+                callback: function () {
+                    //get list of dirty fields
+                    var acctAttributes = formContext.data.entity.attributes.get();
+                    if (acctAttributes != null) {
+                        for (var i in acctAttributes) {
+                            if (acctAttributes[i].getIsDirty()) {
+                                formContext.getAttribute(acctAttributes[i].getName()).setSubmitMode("never");
+                            }
+                        }
+                        setTimeout(function () { formContext.ui.close(); }, 1000);
+                    }
+                },
+                setFocus: false,
+                preventClose: false
+            }
+        ],
+        'Warning', 600, 250, '', true);
+}
