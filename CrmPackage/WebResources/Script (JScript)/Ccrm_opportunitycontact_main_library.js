65d4b8d9219e6e51a172cdda16512ffbea68183c
Shruti Patil <shruti.patil@arup.com>
2020-09-25 12:40:27 +0100

    Replaced XRM.Page with formContext

--- CrmPackage/WebResources/Script (JScript)/Ccrm_opportunitycontact_main_library.js
+++ CrmPackage/WebResources/Script (JScript)/Ccrm_opportunitycontact_main_library.js
@@ -1,18 +1,20 @@
-function Form_onsave() {
+function Form_onsave(executionContext) {
+    formContext = executionContext.getFormContext();
+
     //force submit on readonly fields
-    Xrm.Page.getAttribute("ccrm_firstname").setSubmitMode("always");
-    Xrm.Page.getAttribute("ccrm_lastname").setSubmitMode("always");
-    Xrm.Page.getAttribute("ccrm_phone").setSubmitMode("always");
-    Xrm.Page.getAttribute("ccrm_email").setSubmitMode("always");
-    Xrm.Page.getAttribute("ccrm_organisationid").setSubmitMode("always");
+    formContext.getAttribute("ccrm_firstname").setSubmitMode("always");
+    formContext.getAttribute("ccrm_lastname").setSubmitMode("always");
+    formContext.getAttribute("ccrm_phone").setSubmitMode("always");
+    formContext.getAttribute("ccrm_email").setSubmitMode("always");
+    formContext.getAttribute("ccrm_organisationid").setSubmitMode("always");
 }
 
-ccrm_userid_onchange = function () {
-
+ccrm_userid_onchange = function (executionContext) {
+    formContext = executionContext.getFormContext();
     var userName = '';
-    if (Xrm.Page.getAttribute("ccrm_contactid").getValue() == null && Xrm.Page.getAttribute("ccrm_userid").getValue() != null) {
+    if (formContext.getAttribute("ccrm_contactid").getValue() == null && formContext.getAttribute("ccrm_userid").getValue() != null) {
         var dataset = "SystemUserSet";
-        var retrievedreq = CCrm.JSCore.RetrieveRequest(Xrm.Page.getAttribute("ccrm_userid").getValue()[0].id, dataset);
+        var retrievedreq = CCrm.JSCore.RetrieveRequest(formContext.getAttribute("ccrm_userid").getValue()[0].id, dataset);
         var nodeFirstName = retrievedreq.FirstName;
         var nodeLastName = retrievedreq.LastName;
         var nodeTelephone = retrievedreq.Address1_Telephone1;
@@ -20,64 +22,65 @@ ccrm_userid_onchange = function () {
 
         //firstname
         if (nodeFirstName != null) {
-            Xrm.Page.getAttribute("ccrm_firstname").setValue(nodeFirstName);
+            formContext.getAttribute("ccrm_firstname").setValue(nodeFirstName);
             userName += nodeFirstName;
         }
         else {
-            Xrm.Page.getAttribute("ccrm_firstname").setValue(null);
+            formContext.getAttribute("ccrm_firstname").setValue(null);
         }
         //lastname
         if (nodeLastName != null) {
-            Xrm.Page.getAttribute("ccrm_lastname").setValue(nodeLastName);
+            formContext.getAttribute("ccrm_lastname").setValue(nodeLastName);
             userName += ' ' + nodeLastName;
         }
         else {
-            Xrm.Page.getAttribute("ccrm_lastname").setValue(null);
+            formContext.getAttribute("ccrm_lastname").setValue(null);
         }
         //telephone
         if (nodeTelephone != null) {
-            Xrm.Page.getAttribute("ccrm_phone").setValue(nodeTelephone);
+            formContext.getAttribute("ccrm_phone").setValue(nodeTelephone);
         }
         else {
-            Xrm.Page.getAttribute("ccrm_phone").setValue(null);
+            formContext.getAttribute("ccrm_phone").setValue(null);
         }
         //email
         if (nodeEmail != null) {
-            Xrm.Page.getAttribute("ccrm_email").setValue(nodeEmail);
+            formContext.getAttribute("ccrm_email").setValue(nodeEmail);
         }
         else {
-            Xrm.Page.getAttribute("ccrm_email").setValue(null);
+            formContext.getAttribute("ccrm_email").setValue(null);
         }
-        Xrm.Page.getAttribute("ccrm_name").setValue(userName);
+        formContext.getAttribute("ccrm_name").setValue(userName);
 
     }
-    else if (Xrm.Page.getAttribute("ccrm_contactid").getValue() != null && Xrm.Page.getAttribute("ccrm_userid").getValue() != null) {
+    else if (formContext.getAttribute("ccrm_contactid").getValue() != null && formContext.getAttribute("ccrm_userid").getValue() != null) {
         alert("Please select either a User or a Contact. \nRemove the value first before changing to a User or a Contact");
         //null values
-        Xrm.Page.getAttribute("ccrm_userid").setValue(null);
-        Xrm.Page.getAttribute("ccrm_name").setValue(Xrm.Page.getAttribute("ccrm_name").getValue());
-        Xrm.Page.getAttribute("ccrm_firstname").setValue(Xrm.Page.getAttribute("ccrm_firstname").getValue());
-        Xrm.Page.getAttribute("ccrm_lastname").setValue(Xrm.Page.getAttribute("ccrm_lastname").getValue());
-        Xrm.Page.getAttribute("ccrm_phone").setValue(Xrm.Page.getAttribute("ccrm_phone").getValue());
-        Xrm.Page.getAttribute("ccrm_email").setValue(Xrm.Page.getAttribute("ccrm_email").getValue());
+        formContext.getAttribute("ccrm_userid").setValue(null);
+        formContext.getAttribute("ccrm_name").setValue(formContext.getAttribute("ccrm_name").getValue());
+        formContext.getAttribute("ccrm_firstname").setValue(formContext.getAttribute("ccrm_firstname").getValue());
+        formContext.getAttribute("ccrm_lastname").setValue(formContext.getAttribute("ccrm_lastname").getValue());
+        formContext.getAttribute("ccrm_phone").setValue(formContext.getAttribute("ccrm_phone").getValue());
+        formContext.getAttribute("ccrm_email").setValue(formContext.getAttribute("ccrm_email").getValue());
     }
 
-    if (Xrm.Page.getAttribute("ccrm_contactid").getValue() == null && Xrm.Page.getAttribute("ccrm_userid").getValue() == null) {
+    if (formContext.getAttribute("ccrm_contactid").getValue() == null && formContext.getAttribute("ccrm_userid").getValue() == null) {
         //null values
-        Xrm.Page.getAttribute("ccrm_name").setValue(null);
-        Xrm.Page.getAttribute("ccrm_firstname").setValue(null);
-        Xrm.Page.getAttribute("ccrm_lastname").setValue(null);
-        Xrm.Page.getAttribute("ccrm_phone").setValue(null);
-        Xrm.Page.getAttribute("ccrm_email").setValue(null);
-        Xrm.Page.getAttribute("ccrm_organisationid").setValue(null);
+        formContext.getAttribute("ccrm_name").setValue(null);
+        formContext.getAttribute("ccrm_firstname").setValue(null);
+        formContext.getAttribute("ccrm_lastname").setValue(null);
+        formContext.getAttribute("ccrm_phone").setValue(null);
+        formContext.getAttribute("ccrm_email").setValue(null);
+        formContext.getAttribute("ccrm_organisationid").setValue(null);
     }
 }
 
-ccrm_contactid_onchange = function () {
+ccrm_contactid_onchange = function (executionContext) {
+    formContext = executionContext.getFormContext();
     var contactName = '';
-    if (Xrm.Page.getAttribute("ccrm_userid").getValue() == null && Xrm.Page.getAttribute("ccrm_contactid").getValue() != null) {
+    if (formContext.getAttribute("ccrm_userid").getValue() == null && formContext.getAttribute("ccrm_contactid").getValue() != null) {
         var dataset = "ContactSet";
-        var retrievedreq = CCrm.JSCore.RetrieveRequest(Xrm.Page.getAttribute("ccrm_contactid").getValue()[0].id, dataset);
+        var retrievedreq = CCrm.JSCore.RetrieveRequest(formContext.getAttribute("ccrm_contactid").getValue()[0].id, dataset);
         var nodeFirstName = retrievedreq.FirstName;
         var nodeLastName = retrievedreq.LastName;
         var nodeParentOrgId = retrievedreq.ParentCustomerId;
@@ -86,55 +89,55 @@ ccrm_contactid_onchange = function () {
 
         //firstname
         if (nodeFirstName != null) {
-            Xrm.Page.getAttribute("ccrm_firstname").setValue(nodeFirstName);
+            formContext.getAttribute("ccrm_firstname").setValue(nodeFirstName);
             contactName += nodeFirstName;
         }
         else {
-            Xrm.Page.getAttribute("ccrm_firstname").setValue(null);
+            formContext.getAttribute("ccrm_firstname").setValue(null);
         }
         //lastname
         if (nodeLastName != null) {
-            Xrm.Page.getAttribute("ccrm_lastname").setValue(nodeLastName);
+            formContext.getAttribute("ccrm_lastname").setValue(nodeLastName);
             contactName += ' ' + nodeLastName;
         }
         else {
-            Xrm.Page.getAttribute("ccrm_lastname").setValue(null);
+            formContext.getAttribute("ccrm_lastname").setValue(null);
         }
         //telephone
         if (nodeTelephone != null) {
-            Xrm.Page.getAttribute("ccrm_phone").setValue(nodeTelephone);
+            formContext.getAttribute("ccrm_phone").setValue(nodeTelephone);
         }
         else {
-            Xrm.Page.getAttribute("ccrm_phone").setValue(null);
+            formContext.getAttribute("ccrm_phone").setValue(null);
         }
         //email
         if (nodeEmail != null) {
-            Xrm.Page.getAttribute("ccrm_email").setValue(nodeEmail);
+            formContext.getAttribute("ccrm_email").setValue(nodeEmail);
         }
         else {
-            Xrm.Page.getAttribute("ccrm_email").setValue(null);
+            formContext.getAttribute("ccrm_email").setValue(null);
         }
-        Xrm.Page.getAttribute("ccrm_name").setValue(contactName);
+        formContext.getAttribute("ccrm_name").setValue(contactName);
 
     }
-    else if (Xrm.Page.getAttribute("ccrm_contactid").getValue() != null && Xrm.Page.getAttribute("ccrm_userid").getValue() != null) {
+    else if (formContext.getAttribute("ccrm_contactid").getValue() != null && formContext.getAttribute("ccrm_userid").getValue() != null) {
         alert("Please select either a User or a Contact. \nRemove the value first before changing to a User or a Contact.");
         //null values
-        Xrm.Page.getAttribute("ccrm_contactid").setValue(null);
-        Xrm.Page.getAttribute("ccrm_name").setValue(Xrm.Page.getAttribute("ccrm_name").getValue());
-        Xrm.Page.getAttribute("ccrm_firstname").setValue(Xrm.Page.getAttribute("ccrm_firstname").getValue());
-        Xrm.Page.getAttribute("ccrm_lastname").setValue(Xrm.Page.getAttribute("ccrm_lastname").getValue());
-        Xrm.Page.getAttribute("ccrm_phone").setValue(Xrm.Page.getAttribute("ccrm_phone").getValue());
-        Xrm.Page.getAttribute("ccrm_email").setValue(Xrm.Page.getAttribute("ccrm_email").getValue());
+        formContext.getAttribute("ccrm_contactid").setValue(null);
+        formContext.getAttribute("ccrm_name").setValue(formContext.getAttribute("ccrm_name").getValue());
+        formContext.getAttribute("ccrm_firstname").setValue(formContext.getAttribute("ccrm_firstname").getValue());
+        formContext.getAttribute("ccrm_lastname").setValue(formContext.getAttribute("ccrm_lastname").getValue());
+        formContext.getAttribute("ccrm_phone").setValue(formContext.getAttribute("ccrm_phone").getValue());
+        formContext.getAttribute("ccrm_email").setValue(formContext.getAttribute("ccrm_email").getValue());
     }
-    if (Xrm.Page.getAttribute("ccrm_contactid").getValue() == null && Xrm.Page.getAttribute("ccrm_userid").getValue() == null) {
+    if (formContext.getAttribute("ccrm_contactid").getValue() == null && formContext.getAttribute("ccrm_userid").getValue() == null) {
         //null values
-        Xrm.Page.getAttribute("ccrm_name").setValue(null);
-        Xrm.Page.getAttribute("ccrm_firstname").setValue(null);
-        Xrm.Page.getAttribute("ccrm_lastname").setValue(null);
-        Xrm.Page.getAttribute("ccrm_phone").setValue(null);
-        Xrm.Page.getAttribute("ccrm_email").setValue(null);
-        Xrm.Page.getAttribute("ccrm_organisationid").setValue(null);
+        formContext.getAttribute("ccrm_name").setValue(null);
+        formContext.getAttribute("ccrm_firstname").setValue(null);
+        formContext.getAttribute("ccrm_lastname").setValue(null);
+        formContext.getAttribute("ccrm_phone").setValue(null);
+        formContext.getAttribute("ccrm_email").setValue(null);
+        formContext.getAttribute("ccrm_organisationid").setValue(null);
     }
 
     if (nodeParentOrgId != null && nodeParentOrgId.Id != null) {
@@ -142,11 +145,12 @@ ccrm_contactid_onchange = function () {
         getOrganisation(nodeParentOrgId.Id);
     }
     else {
-        Xrm.Page.getAttribute("ccrm_organisationid").setValue(null);
+        formContext.getAttribute("ccrm_organisationid").setValue(null);
     }
 }
 
-getOrganisation = function (nodeParentOrgId) {
+getOrganisation = function (executionContext, nodeParentOrgId) {
+    formContext = executionContext.getFormContext();
     //get organisation
     var dataset = "AccountSet";
     // use the RetrieveRequest function in the library to retrieve the record
@@ -155,15 +159,16 @@ getOrganisation = function (nodeParentOrgId) {
 
     var lookupItem = new Array();
     lookupItem[0] = new LookupControlItem(nodeParentOrgId, 1, nodeOrgName);
-    Xrm.Page.getAttribute("ccrm_organisationid").setValue(lookupItem);
+    formContext.getAttribute("ccrm_organisationid").setValue(lookupItem);
 }
 
-ccrm_name_onchange = function () {
+ccrm_name_onchange = function (executionContext) {
+    formContext = executionContext.getFormContext();
     var fullname = '';
-    if (Xrm.Page.getAttribute("ccrm_firstname").getValue() != null)
-        fullname += Xrm.Page.getAttribute("ccrm_firstname").getValue();
-    if (Xrm.Page.getAttribute("ccrm_lastname").getValue() != null)
-        fullname += Xrm.Page.getAttribute("ccrm_firstname").getValue();
+    if (formContext.getAttribute("ccrm_firstname").getValue() != null)
+        fullname += formContext.getAttribute("ccrm_firstname").getValue();
+    if (formContext.getAttribute("ccrm_lastname").getValue() != null)
+        fullname += formContext.getAttribute("ccrm_firstname").getValue();
 
-    Xrm.Page.getAttribute("ccrm_name").setValue(fullname);
+    formContext.getAttribute("ccrm_name").setValue(fullname);
 }
