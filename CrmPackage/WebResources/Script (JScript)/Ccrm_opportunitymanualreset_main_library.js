fe24735ce402b84a98e8c9eb11e50bcaeb52b011
Tony Suslovich <Tony.Suslovich@arup.com>
2020-09-25 11:29:39 -0400

    #80881

--- CrmPackage/WebResources/Script (JScript)/Ccrm_opportunitymanualreset_main_library.js
+++ CrmPackage/WebResources/Script (JScript)/Ccrm_opportunitymanualreset_main_library.js
@@ -1,93 +1,98 @@
-function Form_onload() {
-
-    disableFieldsOnForm = function () {
-        Xrm.Page.getControl("ccrm_action").setDisabled(true);
-        Xrm.Page.getControl("ccrm_gateway").setDisabled(true);
-        Xrm.Page.getControl("ccrm_setreference").setDisabled(true);
-        Xrm.Page.getControl("ccrm_referencenumber").setDisabled(true);
-    }
-    setActionFields = function () {
-        //set gateway section to hidden
-        Xrm.Page.ui.tabs.get(0).sections.get(2).setVisible(false);
-        //set reference section to hidden
-        Xrm.Page.ui.tabs.get(0).sections.get(3).setVisible(false);
-        //on creation
-        if (Xrm.Page.ui.getFormType() == 1) {
-            //set the values to NULL
-            Xrm.Page.getAttribute("ccrm_gateway").setValue(null);
-            Xrm.Page.getAttribute("ccrm_setreference").setValue(null);
-            Xrm.Page.getAttribute("ccrm_referencenumber").setValue(null);
-        }
-        else {
-            //disable fields on form
-            disableFieldsOnForm();
-        }
-		/*
-			100,000,000 = Set Gateway
-			100,000,001 = Reset Decline Gateway
-			100,000,002 = Set Reference Number
-			100,000,003 = Clear Reference Number
-		*/
-        if (Xrm.Page.getAttribute("ccrm_action").getValue() == '100000000' || Xrm.Page.getAttribute("ccrm_action").getValue() == '100000001') {
-            //show gateway section
-            Xrm.Page.ui.tabs.get(0).sections.get(2).setVisible(true);
-            //set the gateway required
-            Xrm.Page.getAttribute("ccrm_gateway").setRequiredLevel("required");
-            //
-        }
-        else {
-            //hide gateway section
-            Xrm.Page.ui.tabs.get(0).sections.get(2).setVisible(false);
-            //set the gateway not required
-            Xrm.Page.getAttribute("ccrm_gateway").setRequiredLevel("none");
-        }
-        if (Xrm.Page.getAttribute("ccrm_action").getValue() == '100000002') {
-            //show reference section
-            Xrm.Page.ui.tabs.get(0).sections.get(3).setVisible(true);
-            Xrm.Page.getControl("ccrm_referencenumber").setVisible(true);
-            //set reference required
-            Xrm.Page.getAttribute("ccrm_setreference").setRequiredLevel("required");
-            Xrm.Page.getAttribute("ccrm_referencenumber").setRequiredLevel("required");
-        }
-        else if (Xrm.Page.getAttribute("ccrm_action").getValue() == '100000003') {
-            //show reference section
-            Xrm.Page.ui.tabs.get(0).sections.get(3).setVisible(true);
-            //set reference required
-            Xrm.Page.getAttribute("ccrm_setreference").setRequiredLevel("required");
-            Xrm.Page.getAttribute("ccrm_referencenumber").setRequiredLevel("none");
-            Xrm.Page.getControl("ccrm_referencenumber").setVisible(false);
-        }
-        else {
-            //hide reference section
-            Xrm.Page.ui.tabs.get(0).sections.get(3).setVisible(false);
-            //set reference not required
-            Xrm.Page.getAttribute("ccrm_setreference").setRequiredLevel("none");
-            Xrm.Page.getAttribute("ccrm_referencenumber").setRequiredLevel("none");
-
-        }
-    }
-
+function Form_onload(executionContext) {
 
     //set owner as name
+    var formContext = executionContext.getFormContext();
     var dt = new Date();
-    var strName = Xrm.Page.getAttribute("ownerid").getValue()[0].name + " - " + dt.toLocaleString();
-    Xrm.Page.getAttribute("ccrm_name").setValue(strName);
+    var strName = formContext.getAttribute("ownerid").getValue()[0].name + " - " + dt.toLocaleString();
+    formContext.getAttribute("ccrm_name").setValue(strName);
 
     //hide name if null
-    if (Xrm.Page.getAttribute("ccrm_name").getValue() == null)
-        Xrm.Page.getControl("ccrm_name").setVisible(true);
+    if (formContext.getAttribute("ccrm_name").getValue() == null)
+        formContext.getControl("ccrm_name").setVisible(true);
     else
-        Xrm.Page.getControl("ccrm_name").setVisible(false);
+        formContext.getControl("ccrm_name").setVisible(false);
 
     //show/hide sections
-    setActionFields();
+    setActionFields(executionContext);
 
     //force submit - as name is readonly
-    Xrm.Page.getAttribute("ccrm_name").setSubmitMode("always");
-    Xrm.Page.getAttribute("ccrm_opportunityid").setSubmitMode("always");
+    formContext.getAttribute("ccrm_name").setSubmitMode("always");
+    formContext.getAttribute("ccrm_opportunityid").setSubmitMode("always");
 }
+
+function setActionFields(executionContext) {
+    var formContext = executionContext.getFormContext();
+    //set gateway section to hidden
+    formContext.ui.tabs.get(0).sections.get(2).setVisible(false);
+    //set reference section to hidden
+    formContext.ui.tabs.get(0).sections.get(3).setVisible(false);
+    //on creation
+    if (formContext.ui.getFormType() == 1) {
+        //set the values to NULL
+        formContext.getAttribute("ccrm_gateway").setValue(null);
+        formContext.getAttribute("ccrm_setreference").setValue(null);
+        formContext.getAttribute("ccrm_referencenumber").setValue(null);
+    }
+    else {
+        //disable fields on form
+        disableFieldsOnForm(formContext);
+    }
+    /*
+        100,000,000 = Set Gateway
+        100,000,001 = Reset Decline Gateway
+        100,000,002 = Set Reference Number
+        100,000,003 = Clear Reference Number
+    */
+    if (formContext.getAttribute("ccrm_action").getValue() == '100000000' || formContext.getAttribute("ccrm_action").getValue() == '100000001') {
+        //show gateway section
+        formContext.ui.tabs.get(0).sections.get(2).setVisible(true);
+        //set the gateway required
+        formContext.getAttribute("ccrm_gateway").setRequiredLevel("required");
+        //
+    }
+    else {
+        //hide gateway section
+        formContext.ui.tabs.get(0).sections.get(2).setVisible(false);
+        //set the gateway not required
+        formContext.getAttribute("ccrm_gateway").setRequiredLevel("none");
+    }
+    if (formContext.getAttribute("ccrm_action").getValue() == '100000002') {
+        //show reference section
+        formContext.ui.tabs.get(0).sections.get(3).setVisible(true);
+        formContext.getControl("ccrm_referencenumber").setVisible(true);
+        //set reference required
+        formContext.getAttribute("ccrm_setreference").setRequiredLevel("required");
+        formContext.getAttribute("ccrm_referencenumber").setRequiredLevel("required");
+    }
+    else if (formContext.getAttribute("ccrm_action").getValue() == '100000003') {
+        //show reference section
+        formContext.ui.tabs.get(0).sections.get(3).setVisible(true);
+        //set reference required
+        formContext.getAttribute("ccrm_setreference").setRequiredLevel("required");
+        formContext.getAttribute("ccrm_referencenumber").setRequiredLevel("none");
+        formContext.getControl("ccrm_referencenumber").setVisible(false);
+    }
+    else {
+        //hide reference section
+        formContext.ui.tabs.get(0).sections.get(3).setVisible(false);
+        //set reference not required
+        formContext.getAttribute("ccrm_setreference").setRequiredLevel("none");
+        formContext.getAttribute("ccrm_referencenumber").setRequiredLevel("none");
+
+    }
+}
+
+function disableFieldsOnForm(formContext) {
+    formContext.getControl("ccrm_action").setDisabled(true);
+    formContext.getControl("ccrm_gateway").setDisabled(true);
+    formContext.getControl("ccrm_setreference").setDisabled(true);
+    formContext.getControl("ccrm_referencenumber").setDisabled(true);
+}
+
 function Form_onsave(executionObj) {
-    if (Xrm.Page.getAttribute("ccrm_opportunityid").getValue() == null) {
+    var formContext = executionObj.getFormContext();
+
+    if (formContext.getAttribute("ccrm_opportunityid").getValue() == null) {
         alert("A Manual Reset can only be requested from within an Opportunity.");
         executionObj.getEventArgs().preventDefault();
         return false;
