e2cbae77a212ef3f2a9ff77a34e3e37e2a76a77b
Shruti Patil <shruti.patil@arup.com>
2021-01-29 17:09:26 +0000

    close_opportunty.html : isFramework == 'true' getopportunityreason= modified filter criteria changed some label

--- CrmPackage/WebResources/Script (JScript)/Opportunity/arup_Opportunity Changes.js
+++ CrmPackage/WebResources/Script (JScript)/Opportunity/arup_Opportunity Changes.js
@@ -97,10 +97,10 @@ function getOpportunityReasons(ClientUrl, activeStageId, statusCode, arupInterna
     var dictionary = {};
     var req = new XMLHttpRequest();
     var isFrameworkOpportunity = (formContext.getAttribute("arup_opportunitytype").getValue() == '770000003') ? true : false;
-    if (isFrameworkOpportunity)
-        req.open("GET", ClientUrl + "/api/data/v9.1/arup_closeopportunityreasons?$select=arup_lostreasons,arup_frameworkwonopportunityreason&$filter=ccrm_stageid eq '" + activeStageId + "' and  arup_arupinternalopportunity eq " + arupInternal + " and  arup_isframeworkopportunity eq " + isFrameworkOpportunity + " and  arup_closeframework eq " + isCloseFrameWork, false);
-    else
-        req.open("GET", ClientUrl + "/api/data/v9.1/arup_closeopportunityreasons?$select=arup_lostreasons,arup_wonreasons&$filter=ccrm_stageid eq '" + activeStageId + "' and  arup_arupinternalopportunity eq " + arupInternal , false);
+   // if (isFrameworkOpportunity)
+    req.open("GET", ClientUrl + "/api/data/v9.1/arup_closeopportunityreasons?$select=arup_lostreasons,arup_wonreasons,arup_frameworkwonopportunityreason&$filter=ccrm_stageid eq '" + activeStageId + "' and  arup_arupinternalopportunity eq " + arupInternal + " and  arup_isframeworkopportunity eq " + isFrameworkOpportunity + " and  arup_closeframework eq " + isCloseFrameWork, false);
+    //else
+    //    req.open("GET", ClientUrl + "/api/data/v9.1/arup_closeopportunityreasons?$select=arup_lostreasons,arup_wonreasons&$filter=ccrm_stageid eq '" + activeStageId + "' and  arup_arupinternalopportunity eq " + arupInternal , false);
 
     req.setRequestHeader("OData-MaxVersion", "4.0");
     req.setRequestHeader("OData-Version", "4.0");
