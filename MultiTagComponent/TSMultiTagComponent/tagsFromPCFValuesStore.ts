130a4d17ec0cb839b844f4a92cf3f372946b3268
Roger Hill <hill.roger@gmail.com>
2021-02-04 10:12:02 +0000

    Ensure that we respect the state of PCF control values (Active/Inactive)

--- MultiTagComponent/TSMultiTagComponent/tagsFromPCFValuesStore.ts
+++ MultiTagComponent/TSMultiTagComponent/tagsFromPCFValuesStore.ts
@@ -14,13 +14,17 @@ export class TagsFromPCFValuesStore implements TagValueSource {
         this._pcfDependentEntity = entityName;
     }
 
+    /**
+     * Returns a Promise that will be resolved when the list of tag values is available (fetched asynchronously from the CRM server)
+     * @param context PCF context value.
+     */
     public getAvailableTagValues(context: ComponentFramework.Context<IInputs>): Promise<string> {
         // Has a semicolon separated list of services for which we will obtain available tag values
         const services = context.parameters.DependentField.raw;
 
         return new Promise<string>((resolve, reject) => {
             if (this._currentDependentFieldValue.localeCompare(services ?? "") == 0) {
-                // Dependent field value unchanges, so return existing tag value list.
+                // Dependent field value unchanged, so return existing tag value list.
                 resolve(this._availableTagValues);
             } else {
                 // The dependent field contains a semicolon separated list of values. 
@@ -38,7 +42,7 @@ export class TagsFromPCFValuesStore implements TagValueSource {
                         map(function (val: string) { return "arup_pcfdependentfieldvalue eq '" + encodeURIComponent(val) + "'"; }).
                         join(" or ");
 
-                    let queryString: string = "?$select=arup_pcfvalues&$filter=arup_dependententity eq '" + pcfDependentEntity + "' and arup_name eq '" + pcfDependentFieldName + "' and (" + dependentFieldValueFilter + ")";
+                    let queryString: string = "?$select=arup_pcfvalues&$filter=arup_dependententity eq '" + pcfDependentEntity + "' and arup_name eq '" + pcfDependentFieldName + "' and (" + dependentFieldValueFilter + ") and statecode eq 0";
 
                     context.webAPI.retrieveMultipleRecords(this._pcfValueStoreEntity, queryString).then(
                         (response) => {
