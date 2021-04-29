a8d41780a5926c72d9e41cdcf814007b5456822c
Raul Fernandes <Raul.Fernandes@arup.com>
2021-04-29 14:57:28 +0100

    updated Connection file

--- CrmPackage/WebResources/Script (JScript)/All Other Entities/arup_connection.js
+++ CrmPackage/WebResources/Script (JScript)/All Other Entities/arup_connection.js
@@ -1,79 +1,79 @@
 ﻿var globalContext = Xrm.Utility.getGlobalContext();
+var connectedFromArray;
+var connectedToArray;
 
 function formOnLoad(executionContext) {
     formContext = executionContext.getFormContext();
+
     //check if the connected From is Filled in
     var connectedFrom = formContext.getAttribute("record1id").getValue();
-    if (connectedFrom != null) {      
-        if (connectedFrom[0].entityType == "contact") {
-            formContext.ui.tabs.get("info").sections.get("info_section_1").setVisible(false);
-            formContext.getControl('record2id').addPreSearch(function () {
-                formContext.getControl("record2id").setEntityTypes(["systemuser"]);
-            });
-            formContext.getControl('record1id').addPreSearch(function () {
-                formContext.getControl("record1id").setEntityTypes(["contact"]);
-            });
-        }
+    if (connectedFrom != null) {
+        formContext.ui.tabs.get("info").sections.get("info_section_1").setVisible(false);
 
         setConnectionValues(formContext);
+
+        if (connectedFrom[0].entityType == "contact")
+            filterLookups(formContext);
+
+        displayFormSections(formContext, true);
     } else {
-        displayFormSections(formContext);
+        connectedFromArray = formContext.getControl("record1id").getEntityTypes();
+        connectedToArray = formContext.getControl("record2id").getEntityTypes();
+        displayFormSections(formContext, false);
     }
 }
 
-function displayFormSections(formContext) {
-    //Check if the Option set is null, if so, hide the form sections
-    var connectContacttoUser = formContext.getAttribute("arup_connectingusertocontact").getValue();
-    if (connectContacttoUser == null) {
-        formContext.ui.tabs.get("info").sections.get("info_section_2").setVisible(false);
-        formContext.ui.tabs.get("info").sections.get("info_section_3").setVisible(false);
-        formContext.ui.tabs.get("info").sections.get("info_section_4").setVisible(false);
-        formContext.ui.tabs.get("info").sections.get("description").setVisible(false);
-
-    } else {
-        formContext.ui.tabs.get("info").sections.get("info_section_2").setVisible(true);
-        formContext.ui.tabs.get("info").sections.get("info_section_3").setVisible(true);
-        formContext.ui.tabs.get("info").sections.get("info_section_4").setVisible(true);
-        formContext.ui.tabs.get("info").sections.get("description").setVisible(true);
-    }
+function displayFormSections(formContext, displayValue) {
+    formContext.ui.tabs.get("info").sections.get("info_section_2").setVisible(displayValue);
+    formContext.ui.tabs.get("info").sections.get("info_section_3").setVisible(displayValue);
+    formContext.ui.tabs.get("info").sections.get("info_section_4").setVisible(displayValue);
+    formContext.ui.tabs.get("info").sections.get("description").setVisible(displayValue);
 }
 
 function isConnecting_User_to_Contact(executionContext) {
     var formContext = executionContext.getFormContext();
 
-    displayFormSections(formContext);
     //if the value is yes, connect user to the Contact
     var connectContacttoUser = formContext.getAttribute("arup_connectingusertocontact").getValue();
-    if (connectContacttoUser != null && connectContacttoUser == 1) {
+    if (connectContacttoUser == 1) {
         setConnectionValues(formContext);
-
-        formContext.getControl('record1id').addPreSearch(function () {
-            formContext.getControl("record1id").setEntityTypes(["contact"]);
-        });
-        formContext.getControl('record2id').addPreSearch(function () {
-            formContext.getControl("record2id").setEntityTypes(["systemuser"]);
-        });
-    //} else {
-    //    formContext.getControl('record1id').removePreSearch(function () {
-    //        formContext.getControl("record1id").setEntityTypes(["account"]);
-    //        formContext.getControl("record1id").setEntityTypes(["systemuser"]);
-    //    });
+        filterLookups(formContext);
+    } else if (connectContacttoUser == 0) {
+        removeFilterLookups(formContext);
     }
-}
 
+    displayFormSections(formContext, true);
+}
 
 function setConnectionValues(formContext) {
     Xrm.WebApi.online.retrieveRecord("connectionrole", "f42aa3aa-fd3a-e211-8e0f-005056af0014", "?$select=connectionroleid,name").then(
         function success(result) {
             SetLookupField(formContext, result["connectionroleid"], result["name"], 'connectionrole', 'record1roleid');
             SetLookupField(formContext, result["connectionroleid"], result["name"], 'connectionrole', 'record2roleid');
-            formContext.getAttribute("arup_connectingusertocontact").setValue(1);
         },
         function (error) {
             Xrm.Navigation.openAlertDialog(error.message);
         }
     );
     SetLookupField(formContext, globalContext.userSettings.userId, globalContext.userSettings.userName, 'systemuser', 'record2id');
+}
+
+function filterLookups(formContext) {
+    formContext.getControl('record1id').addPreSearch(function () {
+        formContext.getControl("record1id").setEntityTypes(["contact"]);
+    });
+    formContext.getControl('record2id').addPreSearch(function () {
+        formContext.getControl("record2id").setEntityTypes(["systemuser"]);
+    });
+}
+
+function removeFilterLookups(formContext) {
+    formContext.getControl('record1id').addPreSearch(function () {
+        formContext.getControl("record1id").setEntityTypes(connectedFromArray);
+    });
+    formContext.getControl('record2id').addPreSearch(function () {
+        formContext.getControl("record2id").setEntityTypes(connectedToArray);
+    });
 }
 
 function SetLookupField(formContext, id, name, entity, field) {
